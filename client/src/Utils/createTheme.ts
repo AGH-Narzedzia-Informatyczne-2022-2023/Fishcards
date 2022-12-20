ee797e57844997eb2036ecdc037b5e17a969bcf3
ranz7 <rdslw@student.agh.edu.pl>
2022-12-20 14:11:47 +0100

    Starting from scratch

--- client/src/Utils/createTheme.ts
+++
@@ -1,8 +0,0 @@
-import { merge } from "lodash";
-import { MockTheme, Theme } from "../types/themeTypes";
-import { defaultTheme } from "./themes";
-
-const createTheme = (newTheme: MockTheme): Theme => {
-  return merge(defaultTheme, newTheme);
-};
-export default createTheme;
--- client/src/types/types.d.ts
+++
@@ -1,8 +0,0 @@
-import { Theme } from "./themeTypes";
-import "styled-components";
-
-type CustomTheme = typeof Theme;
-
-declare module "styled-components" {
-  export interface DefaultTheme extends CustomTheme {}
-}
