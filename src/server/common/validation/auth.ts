61ea1e5080b0794f8a97f9a819b922d8b8a63aab
ranz7 <rdslw@student.agh.edu.pl>
2023-01-03 00:40:45 +0100

    Add postgresql migration

--- src/server/common/validation/auth.ts
+++ src/server/common/validation/auth.ts
@@ -1,13 +1,13 @@
 import * as z from "zod";
 
-export const loginSchema = z.object({
+export const signInSchema = z.object({
   email: z.string().email(),
   password: z.string().min(4).max(12),
 });
 
-export const signUpSchema = loginSchema.extend({
+export const signUpSchema = signInSchema.extend({
   username: z.string(),
 });
 
-export type ILogin = z.infer<typeof loginSchema>;
+export type ISignIn = z.infer<typeof signInSchema>;
 export type ISignUp = z.infer<typeof signUpSchema>;
